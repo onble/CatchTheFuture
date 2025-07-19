e58f022b05c7f7c57f45a72ab5be311777de3060
onble <62949816+onble@users.noreply.github.com>
2025-07-19 23:47:10 +0800

    限制篮筐的移动范围

--- assets/scripts/Move.ts
+++ assets/scripts/Move.ts
@@ -3,6 +3,12 @@ const { ccclass, property } = _decorator;
 
 @ccclass("Move")
 export class Move extends Component {
+    @property(Vec2)
+    minBoundary: Vec2 = new Vec2();
+
+    @property(Vec2)
+    maxBoundary: Vec2 = new Vec2();
+
     private _offset: Vec2 = new Vec2();
     private _isDragging: boolean = false;
     protected onLoad(): void {
@@ -22,14 +28,25 @@ export class Move extends Component {
         if (!this._isDragging) return;
         let touchLocation = event.getLocation(); // 手指触摸的位置
         let newLocation = new Vec3(touchLocation.x - this._offset.x, touchLocation.y - this._offset.y);
+        newLocation = this.clampPosition(newLocation);
         this.node.setPosition(newLocation);
     }
-    onTouchEnd(event: EventTouch) {}
+    onTouchEnd(event: EventTouch) {
+        this._isDragging = false;
+    }
     addTouch() {
         this.node.on(Node.EventType.TOUCH_START, this.onTouchStart, this);
         this.node.on(Node.EventType.TOUCH_MOVE, this.onTouchMove, this);
         this.node.on(Node.EventType.TOUCH_END, this.onTouchEnd, this);
         this.node.on(Node.EventType.TOUCH_CANCEL, this.onTouchEnd, this);
+    }
+    clampPosition(position: Vec3): Vec3 {
+        // 将位置x限制在最小边界和最大边界之间
+        let clampedX = Math.max(this.minBoundary.x, Math.min(position.x, this.maxBoundary.x));
+        // 将位置Y限制在最小边界和最大边界之间
+        let clampedY = Math.max(this.minBoundary.y, Math.min(position.y, this.maxBoundary.y));
+        // 返回新的Vec3位置，其中X和Y都被限制在边界内，Z保持不变
+        return new Vec3(clampedX, clampedY, position.z);
     }
     removeTouch() {
         this.node.off(Node.EventType.TOUCH_START, this.onTouchStart, this);
